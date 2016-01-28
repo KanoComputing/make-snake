d54df1f22a6fb506c8e4f0dcd2f6ae40892182e3
Costas Voloudakis <costas@kano.me>
2016-01-28 21:44:50 +0000

    Delegate profile update to kano-profile
    This should avoid deleting the unused variables at the end of the
    game

--- snake/gamestate.py
+++ snake/gamestate.py
@@ -1,37 +1,33 @@
-#!/usr/bin/env python
-
 # gamestate.py
 #
-# Copyright (C) 2013, 2014 Kano Computing Ltd.
-# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+# Copyright (C) 2013-2016 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
+from kano_profile.badges import update_upwards_with_dialog, \
+    increment_app_state_variable_with_dialog
 
-from kano_profile.apps import load_app_state, get_gamestate_variables
-from kano_profile.badges import save_app_state_with_dialog
-
-app_name = 'make-snake'
-state = dict()
-
-
-def load_state():
-    global state
-
-    try:
-        state = load_app_state(app_name)
-        init_states = get_gamestate_variables(app_name)
-
-        # loop through all states and initialise them with zero
-        for s in init_states:
-            if s not in state:
-                state[s] = 0
-    except Exception:
-        pass
+STATUS = {
+    'longest_snake': 0,
+    'total_length': 0,
+    'total_number_of_apples': 0,
+    'total_score': 0,
+    'highest_score': 0
+}
 
 
-def save_state():
-    global state
-
-    try:
-        save_app_state_with_dialog(app_name, state)
-    except Exception:
-        pass
+def update_profile_stats():
+    update_upwards_with_dialog(
+        'make-snake', 'longest_snake', STATUS['longest_snake']
+    )
+    update_upwards_with_dialog(
+        'make-snake', 'highest_score', STATUS['highest_score']
+    )
+    increment_app_state_variable_with_dialog(
+        'make-snake', 'total_length', STATUS['total_length']
+    )
+    increment_app_state_variable_with_dialog(
+        'make-snake', 'total_number_of_apples', STATUS['total_number_of_apples']
+    )
+    increment_app_state_variable_with_dialog(
+        'make-snake', 'total_score', STATUS['total_score']
+    )
