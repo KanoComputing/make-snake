60fa65cfcbd73e691d7911c333fa96d82904301c
Alex Burr <alex.burr@kano.me>
2016-06-10 19:11:04 +0100

    Fix sharing: some uses of '_' were breaking i18n.

--- snake/utils.py
+++ snake/utils.py
@@ -32,8 +32,8 @@ def print_themes(show_default=True, terminate=True):
         custom_themes += '{{2' + l + "}} | "
     custom_themes = custom_themes[:-2]
     # Print info
-    default_themes, _, _ = run_cmd('colour_echo "%s"' % default_themes)
-    custom_themes, _, _ = run_cmd('colour_echo "%s"' % custom_themes)
+    default_themes, junk1, junk2 = run_cmd('colour_echo "%s"' % default_themes)
+    custom_themes, junk1, junk2 = run_cmd('colour_echo "%s"' % custom_themes)
     # Print default themes
     print "\n"
     if show_default:
@@ -51,7 +51,7 @@ def check_valid_theme(theme):
     if not theme.endswith('.xml'):
         theme += '.xml'
     if theme not in theme_list:
-        coloured_error, _, _ = run_cmd('colour_echo "{{8 x }} {{7 error: }}"')
+        coloured_error, junk1, junk2 = run_cmd('colour_echo "{{8 x }} {{7 error: }}"')
         print "\n    " + coloured_error.strip('\n') + \
               _('Theme %s not found (names are case sensitive)\n') % theme
         exit(2)
@@ -60,13 +60,13 @@ def check_valid_theme(theme):
 def share_theme():
     # Check for internet
     if not is_internet():
-        coloured_error, _, _ = run_cmd('colour_echo "{{8 x }} {{7 error: }}"')
+        coloured_error, junk1, junk2 = run_cmd('colour_echo "{{8 x }} {{7 error: }}"')
         print "\n    " + coloured_error.strip('\n') + _('You need internet connection')
         exit(2)
     # Check for login
-    success, _ = login_using_token()
+    success, junk2 = login_using_token()
     if not success:
-        coloured_error, _, _ = run_cmd('colour_echo "{{8 x }} {{7 error: }}"')
+        coloured_error, junk1, junk2 = run_cmd('colour_echo "{{8 x }} {{7 error: }}"')
         print "\n    " + coloured_error.strip('\n') + _('You need to login to Kano World')
         exit(2)
     # Print themes
@@ -97,10 +97,10 @@ def share_theme():
     filepath = os.path.join(app_dir, theme)
     success, msg = upload_share(filepath, title, 'make-snake')
     if not success:
-        coloured_error, _, _ = run_cmd('colour_echo "{{8 x }} {{7 error: }}"')
+        coloured_error, junk1, junk2 = run_cmd('colour_echo "{{8 x }} {{7 error: }}"')
         print "\n    " + coloured_error.strip('\n') + _('Sharing of {} failed. {}\n') % (theme, msg)
         exit(2)
-    message, _, _ = run_cmd('colour_echo "{{4 + }} {{3 You have shared your theme successfully }}"')
+    message, junk1, junk2 = run_cmd('colour_echo "{{4 + }} {{3 You have shared your theme successfully }}"')
     print "\n    " + message.strip('\n')
     increment_app_state_variable_with_dialog('make-snake', 'shared', 1)
     exit(2)
